d67de55dff7a331ea68120e495ce322fd3057e62
isidoreduc <isidoreducasse79@gmail.com>
2019-07-17 12:46:06 +0200

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 // named import need braces as opposed to default imports
 // when referencing index.js from a folder, you only need to state the folder, webpack will default to index.js automatically
-import { selectSong } from '../actions';
+
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className='ui container grid'>
+      <div className='ui row'>
+        <div className='column eight wide'>
+          <SongList />
+        </div>
+        <div className='column eight wide'>
+          <SongDetail />
+        </div>
+      </div>
     </div>
   );
 };
