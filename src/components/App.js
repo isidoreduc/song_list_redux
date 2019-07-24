555a9a0cb56b99bc37db2c4aec8776fff724a879
isidoreduc <isidoreducasse79@gmail.com>
2019-07-24 20:03:21 +0200

    final

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import SongDetail from './SongDetail';
 
 const App = () => {
   return (
-    <div className='ui container grid'>
+    <div className='ui container grid' style={{ marginTop: '20px' }}>
       <div className='ui row'>
         <div className='column eight wide'>
           <SongList />
